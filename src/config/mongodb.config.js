78507d3c94d888f2eff668ae299ad971c8a6550c
skilltopper <skilltopper1@gmail.com>
2025-12-27 00:18:22 +0500

    Update mongodb.config.js

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  console.log(process.env.MONGODB_URI)
   try {
     const conn = await mongoose.connect(
       `${process.env.MONGODB_URI}/blog-project`
