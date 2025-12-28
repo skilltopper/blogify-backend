549a56e1f5137a7c1442ac09934bb1543e796274
skilltopper <skilltopper1@gmail.com>
2025-12-28 10:14:21 +0500

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -50,6 +50,18 @@ const createUser = async (req, res) => {
     maxAge: 30 * 60 * 1000, // 30 minutes
   });
 
+      await transporter.sendMail({
+    from: `"Blog App" <${process.env.SEMDER_EMAIL}>`,
+    to: email,
+    subject: "Welcome to Blog App",
+    html: `
+      <h1>Welcome to Blog App</h1>
+      <p>Hi ${name},</p>
+      <p>Thank you for registering at Blog App. We are excited to have you on board!</p>
+      <p>Best regards,<br/>Blog App Team</p>
+    `,
+  });
+  
   res.status(201).json(
     new ApiResponse(201, "User created successfully", {
       user: {
@@ -63,17 +75,6 @@ const createUser = async (req, res) => {
     })
   );
 
-    await transporter.sendMail({
-    from: `"Blog App" <${process.env.SEMDER_EMAIL}>`,
-    to: email,
-    subject: "Welcome to Blog App",
-    html: `
-      <h1>Welcome to Blog App</h1>
-      <p>Hi ${name},</p>
-      <p>Thank you for registering at Blog App. We are excited to have you on board!</p>
-      <p>Best regards,<br/>Blog App Team</p>
-    `,
-  });
 };
 
 const loginUser = async (req, res) => {
@@ -113,6 +114,18 @@ const loginUser = async (req, res) => {
     maxAge: 30 * 60 * 1000, // 30 minutes
   });
 
+      await transporter.sendMail({
+    from: `"Blog App" <${process.env.SEMDER_EMAIL}>`,
+    to: email,
+    subject: "Login Notification",
+    html: `
+      <h1>Login Notification</h1>
+      <p>Hi ${user.name},</p>
+      <p>We noticed a login to your account. If this was you, you can safely ignore this email. If you did not log in, please reset your password immediately.</p>
+      <p>Best regards,<br/>Blog App Team</p>
+    `,
+  });
+
   res.status(201).json(
     new ApiResponse(201, "User logged in successfully", {
       user: {
@@ -126,17 +139,6 @@ const loginUser = async (req, res) => {
     })
   );
 
-    await transporter.sendMail({
-    from: `"Blog App" <${process.env.SEMDER_EMAIL}>`,
-    to: email,
-    subject: "Login Notification",
-    html: `
-      <h1>Login Notification</h1>
-      <p>Hi ${user.name},</p>
-      <p>We noticed a login to your account. If this was you, you can safely ignore this email. If you did not log in, please reset your password immediately.</p>
-      <p>Best regards,<br/>Blog App Team</p>
-    `,
-  });
 };
 
 const logoutUser = async (req, res) => {
